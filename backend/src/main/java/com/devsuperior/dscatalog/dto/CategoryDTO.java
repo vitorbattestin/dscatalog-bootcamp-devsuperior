098008777e0e09b20bccc7636c92e621fd7b7ada
Vitor Battestin <vitor.battestin@gmail.com>
2021-05-14 22:24:24 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -5,6 +5,7 @@ import com.devsuperior.dscatalog.entities.Category;
 import java.io.Serializable;
 
 public class CategoryDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private Long id;
     private String name;
