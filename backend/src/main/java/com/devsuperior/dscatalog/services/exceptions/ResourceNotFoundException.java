cfd67775f01584ac220e0ba889503b3f3f6c4037
Vitor Battestin <vitor.battestin@gmail.com>
2021-06-04 16:49:16 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dscatalog.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-
     public ResourceNotFoundException(String msg){
         super(msg);
     }
